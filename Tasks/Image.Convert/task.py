035e28126d614cf916adf6276f0b40a114cf2bbf
Günther Eberl <guenther@eberl.se>
2017-12-22 11:24:36 +0100

    Better conversion for transparent images
    Introduced new optional kwarg: background
    
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/Image.Convert/task.py
+++ Tasks/Image.Convert/task.py
@@ -13,6 +13,9 @@ sys.path.append(os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, 'Dr
 from file_tools import get_file_paths_from_directory
 
 
+FILE_FORMATS_WITH_TRANSPARENCY = ['gif', 'png', 'xpm']
+
+
 class Task(object):
     """
     Documentation: https://docs.droppyapp.com/tasks/image-convert
@@ -20,11 +23,12 @@ class Task(object):
     def __init__(self, input_paths, output_dir, **kwargs):
         # Get keyword arguments.
         extension = kwargs.get(str('extension'), 'png')
+        background = kwargs.get(str('background'), [255, 255, 255])
 
         # Process files and directories.
         for input_path in input_paths:
             if os.path.isfile(input_path):
-                self.convert_file(input_path, output_dir, extension)
+                self.convert_file(input_path, output_dir, extension, background)
 
             elif os.path.isdir(input_path):
                 output_sub_dir = os.path.join(output_dir, os.path.basename(input_path))
@@ -32,20 +36,22 @@ class Task(object):
 
                 contained_files = get_file_paths_from_directory(input_path)
                 for contained_file in contained_files:
-                    self.convert_file(contained_file, output_sub_dir, extension)
+                    self.convert_file(contained_file, output_sub_dir, extension, background)
 
     @staticmethod
-    def convert_file(input_file, output_dir, extension):
-        output_file_name, _ = os.path.splitext(os.path.basename(input_file))
-        output_file = os.path.join(output_dir, output_file_name + '.' + extension)
+    def convert_file(input_file, output_dir, output_extension, bg_replacement):
+        input_file_name, input_extension = os.path.splitext(os.path.basename(input_file))
+        output_file = os.path.join(output_dir, input_file_name + '.' + output_extension)
 
-        input_image = Image.open(input_file)
+        input_image = Image.open(input_file).convert('RGBA')  # always load with alpha channel
 
-        # Discard transparency for file formats that don't support it.
-        if extension.lower() in ['jpg', 'jpeg', 'tiff', 'bmp']:
-            print('Discarding transparent parts.')
-            non_transparent_image = Image.new('RGB', input_image.size, (255, 255, 255, 255))
-            non_transparent_image.paste(input_image, (0, 0), input_image)
+        # Replace transparency when converting from file formats that support it to ones that don't.
+        if output_extension.lower() not in FILE_FORMATS_WITH_TRANSPARENCY:
+            print('Replacing transparent parts with color %s.' % bg_replacement)
+            bg_image = Image.new('RGBA', input_image.size, (bg_replacement[0], bg_replacement[1], bg_replacement[2]))
+
+            non_transparent_image = Image.alpha_composite(bg_image, input_image)
+            non_transparent_image = non_transparent_image.convert('RGB')
 
             non_transparent_image.save(output_file)
         else:
