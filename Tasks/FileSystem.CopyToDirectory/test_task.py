b7c81449bb114b7e768a66568452e13c3d0ec94c
Günther Eberl <guenther@eberl.se>
2017-12-25 17:08:35 +0100

    Fixed tests for Task FileSystem.CopyToDirectory
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/FileSystem.CopyToDirectory/test_task.py
+++ Tasks/FileSystem.CopyToDirectory/test_task.py
@@ -3,49 +3,74 @@
 
 from __future__ import unicode_literals
 import os
+import py
 import pytest
+import shutil
 import task
 
-files_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
+files_dir = py.path.local(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
 
 
-def test_init(tmpdir):
-    t = task.Task(input_dir=[],
-                  output_dir='%s' % tmpdir,
-                  directory='~')
+def test_input_empty(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir,
+                  directory='%s' % tmpdir)
 
     assert isinstance(t, object)
 
 
 def test_unfilled_directory_arg(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
     with pytest.raises(SystemExit) as exc_info:
-        t = task.Task(input_dir=[],
-                      output_dir='%s' % tmpdir)
+        t = task.Task(input_dir='%s' % input_dir,
+                      output_dir='%s' % output_dir)
 
     assert exc_info.type == SystemExit
 
 
 def test_passing_files(tmpdir):
-    input_paths = [os.path.join(files_dir, 'böok$ collection', 'pg5903.epub'),
-                   os.path.join(files_dir, 'böok$ collection', 'vom gut und böse.epub')]
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
 
-    t = task.Task(input_dir=input_paths,
-                  output_dir='%s' % tmpdir,
+    shutil.copyfile('%s' % files_dir.join('pg5903.epub'),
+                    '%s' % input_dir.join('pg5903.epub'))
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir,
                   directory='%s' % tmpdir.join('some other dir'),
                   create_directory=True)
 
     assert tmpdir.join('some other dir', 'pg5903.epub').check() is True
-    assert tmpdir.join('some other dir', 'vom gut und böse.epub').check() is True
 
 
 def test_passing_dir(tmpdir):
-    input_paths = [os.path.join(files_dir, 'böok$ collection')]
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
 
-    t = task.Task(input_dir=input_paths,
-                  output_dir='%s' % tmpdir,
+    shutil.copytree('%s' % files_dir,
+                    '%s' % input_dir.join('böok$ collection'))
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir,
                   directory='%s' % tmpdir.join('some other dir'),
                   create_directory=True)
 
     assert tmpdir.join('some other dir', 'böok$ collection').check() is True
     assert tmpdir.join('some other dir', 'böok$ collection', 'pg5903.epub').check() is True
-    assert tmpdir.join('some other dir', 'böok$ collection', 'vom gut und böse.epub').check() is True
