0a02f6c874cbd442e1db2c4f7300549aa60d0fbb
Günther Eberl <guenther@eberl.se>
2017-12-25 18:58:20 +0100

    Fixed tests for Task Image.Convert
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/Image.Convert/test_task.py
+++ Tasks/Image.Convert/test_task.py
@@ -3,24 +3,38 @@
 
 from __future__ import unicode_literals
 import os
+import py
+import shutil
 import task
 
-files_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
+files_dir = py.path.local(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
 
 
-def test_init(tmpdir):
-    t = task.Task(input_dir=[],
-                  output_dir='%s' % tmpdir)
+def test_input_empty(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir)
 
     assert isinstance(t, object)
 
 
-def test_passing_files(tmpdir):
-    input_paths = [os.path.join(files_dir, 'some_subdir', 'IMG_1248.JPG'),
-                   os.path.join(files_dir, 'this_side_up.png')]
+def test_input_file(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
 
-    t = task.Task(input_dir=input_paths,
-                  output_dir='%s' % tmpdir)
+    shutil.copyfile('%s' % files_dir.join('this_side_up.png'),
+                    '%s' % input_dir.join('this_side_up.png'))
 
-    assert tmpdir.join('IMG_1248.png').check() is True
-    assert tmpdir.join('this_side_up.png').check() is True
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir,
+                  extension='jpeg')
+
+    assert output_dir.join('this_side_up.jpeg').check() is True
