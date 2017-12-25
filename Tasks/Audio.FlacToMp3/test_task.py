4ca3be56d2e95de26ef0e1eae3f7b7c66e1a2f1c
Günther Eberl <guenther@eberl.se>
2017-12-25 14:21:56 +0100

    Fixed tests for Task Audio.FlacToMp3
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/Audio.FlacToMp3/test_task.py
+++ Tasks/Audio.FlacToMp3/test_task.py
@@ -3,41 +3,72 @@
 
 from __future__ import unicode_literals
 import os
+import py
 import pytest
+import shutil
 import task
 
-files_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
+files_dir = py.path.local(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
 
 
-def test_init(tmpdir):
-    t = task.Task(input_dir=[],
-                  output_dir='%s' % tmpdir)
+def test_input_empty(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir)
 
     assert isinstance(t, object)
 
 
 def test_external_executable_na(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
     with pytest.raises(SystemExit) as exc_info:
-        t = task.Task(input_dir=[],
-                      output_dir='%s' % tmpdir,
+        t = task.Task(input_dir='%s' % input_dir,
+                      output_dir='%s' % output_dir,
                       flac_executable='/this/path/does/not/exist')
 
     assert exc_info.type == SystemExit
 
 
-def test_passing_files(tmpdir):
-    input_paths = [os.path.join(files_dir, 'äudio collection', 'homer.flac')]
+def test_input_file(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
 
-    t = task.Task(input_dir=input_paths,
-                  output_dir='%s' % tmpdir)
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
 
-    assert tmpdir.join('homer.mp3').check() is True
+    shutil.copyfile('%s' % files_dir.join('homer.flac'),
+                    '%s' % input_dir.join('homer.flac'))
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir)
+
+    assert output_dir.join('homer.mp3').check() is True
 
 
-def test_passing_dir(tmpdir):
-    input_paths = [os.path.join(files_dir, 'äudio collection')]
+def test_input_folder(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
 
-    t = task.Task(input_dir=input_paths,
-                  output_dir='%s' % tmpdir)
+    content_dir = input_dir.join('content')
+    os.makedirs('%s' % content_dir)
 
-    assert tmpdir.join('äudio collection', 'homer.mp3').check() is True
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    shutil.copyfile('%s' % files_dir.join('homer.flac'),
+                    '%s' % content_dir.join('homer.flac'))
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir)
+
+    assert output_dir.join('content', 'homer.mp3').check() is True
