94c58bf3b1d2f5d985d4a953699081712c6a1517
Günther Eberl <guenther@eberl.se>
2017-12-25 17:31:58 +0100

    Fixed tests for Task FileSystem.DeleteOriginals
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/FileSystem.DeleteOriginals/test_task.py
+++ Tasks/FileSystem.DeleteOriginals/test_task.py
@@ -2,34 +2,33 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-import codecs
+import py
 import os
 import task
 
-files_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
+files_dir = py.path.local(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
 
 
-def test_init(tmpdir):
-    original_dir = tmpdir.join('0', 'files')
-    os.makedirs('%s' % original_dir)
+def test_input_empty(tmpdir):
+    input_dir = tmpdir.join('0', 'files')
+    os.makedirs('%s' % input_dir)
 
     output_dir = tmpdir.join('1')
     os.makedirs('%s' % output_dir)
 
-    t = task.Task(input_dir=[],
+    t = task.Task(input_dir='%s' % input_dir,
                   output_dir='%s' % output_dir,
                   delete_to_trash=False)
 
     assert isinstance(t, object)
 
 
-def test_passing_files(tmpdir):
-    original_dir = tmpdir.join('0', 'files')
-    os.makedirs('%s' % original_dir)
+def test_input_file(tmpdir):
+    input_dir = tmpdir.join('0', 'files')
+    os.makedirs('%s' % input_dir)
 
     original_file = tmpdir.join('test_file.txt')
-    with codecs.open('%s' % original_file, 'a'):
-        os.utime('%s' % original_file, None)
+    original_file.write('foo')
 
     symlink_to_original_file = tmpdir.join('0', 'files', 'test_file.txt')
     os.symlink('%s' % original_file, '%s' % symlink_to_original_file)
@@ -39,7 +38,7 @@ def test_passing_files(tmpdir):
 
     assert original_file.check() is True
 
-    t = task.Task(input_dir=[],
+    t = task.Task(input_dir='%s' % input_dir,
                   output_dir='%s' % output_dir,
                   delete_to_trash=False)
 
