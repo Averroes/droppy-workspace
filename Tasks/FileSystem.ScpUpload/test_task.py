6189f99e948e10fbbf2f8639761f2cf5c0fa046d
Günther Eberl <guenther@eberl.se>
2017-12-25 18:23:27 +0100

    Fixed tests for Task FileSystem.ScpUpload
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/FileSystem.ScpUpload/test_task.py
+++ Tasks/FileSystem.ScpUpload/test_task.py
@@ -2,40 +2,85 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+import os
 import pytest
 import task
 
 
-def test_init(tmpdir):
-    t = task.Task(input_dir=[],
-                  output_dir='%s' % tmpdir,
+def test_input_empty(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir,
+                  username='myuser',
                   server_address='localhost',
                   remote_path='/')
 
     assert isinstance(t, object)
 
 
-def test_unfilled_server_address_arg(tmpdir):
+def test_unfilled_username_arg(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
     with pytest.raises(SystemExit) as exc_info:
-        t = task.Task(input_dir=[],
-                      output_dir='%s' % tmpdir,
+        t = task.Task(input_dir='%s' % input_dir,
+                      output_dir='%s' % output_dir,
+                      server_address='localhost',
+                      remote_path='/')
+
+    assert exc_info.type == SystemExit
+
+
+def test_unfilled_server_address_arg(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    with pytest.raises(SystemExit) as exc_info:
+        t = task.Task(input_dir='%s' % input_dir,
+                      output_dir='%s' % output_dir,
+                      username='myuser',
                       remote_path='/')
 
     assert exc_info.type == SystemExit
 
 
 def test_unfilled_remote_path_arg(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
     with pytest.raises(SystemExit) as exc_info:
-        t = task.Task(input_dir=[],
-                      output_dir='%s' % tmpdir)
+        t = task.Task(input_dir='%s' % input_dir,
+                      output_dir='%s' % output_dir,
+                      username='myuser',
+                      server_address='localhost')
 
     assert exc_info.type == SystemExit
 
 
 def test_external_executable_na(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
     with pytest.raises(SystemExit) as exc_info:
-        t = task.Task(input_dir=[],
-                      output_dir='%s' % tmpdir,
+        t = task.Task(input_dir='%s' % input_dir,
+                      output_dir='%s' % output_dir,
                       server_address='localhost',
                       scp_executable='/this/path/does/not/exist/scp',
                       remote_path='/')
