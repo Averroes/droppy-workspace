fb58b635d8e7f37f277008e0a9fed327e4440425
Günther Eberl <guenther@eberl.se>
2017-12-25 14:13:33 +0100

    Fixed tests for utility functions in DropPy.Common
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/DropPy.Common/test_file_tools.py
+++ Tasks/DropPy.Common/test_file_tools.py
@@ -3,11 +3,12 @@
 
 from __future__ import unicode_literals
 import os
+import py
 import pytest
-from shutil import copyfile, copytree
+import shutil
 from file_tools import touch_file, copy_file, copy_tree, get_file_paths_from_directory
 
-files_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
+files_dir = py.path.local(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
 
 
 def test_touch_file(tmpdir):
@@ -19,11 +20,13 @@ def test_touch_file(tmpdir):
 
 
 def test_copy_file(tmpdir):
-    input_dir = tmpdir.join('input')
+    input_dir = tmpdir.join('0')
     os.makedirs('%s' % input_dir)
-    copyfile(os.path.join(files_dir, 'this_side_up.png'), '%s' % input_dir.join('this_side_up.png'))
 
-    output_dir = tmpdir.join('target')
+    shutil.copyfile('%s' % files_dir.join('this_side_up.png'),
+                    '%s' % input_dir.join('this_side_up.png'))
+
+    output_dir = tmpdir.join('1')
     os.makedirs('%s' % output_dir)
 
     copy_file(input_file='%s' % input_dir.join('this_side_up.png'),
@@ -33,13 +36,17 @@ def test_copy_file(tmpdir):
 
 
 def test_copy_file_overwrite_false(tmpdir):
-    input_dir = tmpdir.join('input')
+    input_dir = tmpdir.join('0')
     os.makedirs('%s' % input_dir)
-    copyfile(os.path.join(files_dir, 'this_side_up.png'), '%s' % input_dir.join('this_side_up.png'))
 
-    output_dir = tmpdir.join('target')
+    shutil.copyfile('%s' % files_dir.join('this_side_up.png'),
+                    '%s' % input_dir.join('this_side_up.png'))
+
+    output_dir = tmpdir.join('1')
     os.makedirs('%s' % output_dir)
-    copyfile(os.path.join(files_dir, 'this_side_up.png'), '%s' % output_dir.join('this_side_up.png'))
+
+    shutil.copyfile('%s' % files_dir.join('this_side_up.png'),
+                    '%s' % output_dir.join('this_side_up.png'))
 
     with pytest.raises(SystemExit) as exc_info:
         copy_file(input_file='%s' % input_dir.join('this_side_up.png'),
@@ -50,13 +57,17 @@ def test_copy_file_overwrite_false(tmpdir):
 
 
 def test_copy_file_overwrite_true(tmpdir):
-    input_dir = tmpdir.join('input')
+    input_dir = tmpdir.join('0')
     os.makedirs('%s' % input_dir)
-    copyfile(os.path.join(files_dir, 'this_side_up.png'), '%s' % input_dir.join('this_side_up.png'))
 
-    output_dir = tmpdir.join('target')
+    shutil.copyfile('%s' % files_dir.join('this_side_up.png'),
+                    '%s' % input_dir.join('this_side_up.png'))
+
+    output_dir = tmpdir.join('1')
     os.makedirs('%s' % output_dir)
-    copyfile(os.path.join(files_dir, 'this_side_up.png'), '%s' % output_dir.join('this_side_up.png'))
+
+    shutil.copyfile('%s' % files_dir.join('this_side_up.png'),
+                    '%s' % output_dir.join('this_side_up.png'))
 
     copy_file(input_file='%s' % input_dir.join('this_side_up.png'),
               output_file='%s' % output_dir.join('this_side_up.png'),
@@ -66,28 +77,33 @@ def test_copy_file_overwrite_true(tmpdir):
 
 
 def test_copy_tree(tmpdir):
-    input_dir = tmpdir.join('input')
+    input_dir = tmpdir.join('0')
     os.makedirs('%s' % input_dir)
-    copytree(os.path.join(files_dir, 'böok$ collection'), '%s' % input_dir.join('böok$ collection'))
 
-    output_dir = tmpdir.join('target')
+    shutil.copytree('%s' % files_dir,
+                    '%s' % input_dir.join('böok$ collection'))
+
+    output_dir = tmpdir.join('1')
     os.makedirs('%s' % output_dir)
 
     copy_tree(input_dir='%s' % input_dir.join('böok$ collection'),
               output_dir='%s' % output_dir.join('böok$ collection'))
 
     assert output_dir.join('böok$ collection').check() is True
-    assert output_dir.join('böok$ collection', 'pg5903.epub').check() is True
 
 
 def test_copy_tree_overwrite_false(tmpdir):
-    input_dir = tmpdir.join('input')
+    input_dir = tmpdir.join('0')
     os.makedirs('%s' % input_dir)
-    copytree(os.path.join(files_dir, 'böok$ collection'), '%s' % input_dir.join('böok$ collection'))
 
-    output_dir = tmpdir.join('target')
+    shutil.copytree('%s' % files_dir,
+                    '%s' % input_dir.join('böok$ collection'))
+
+    output_dir = tmpdir.join('1')
     os.makedirs('%s' % output_dir)
-    copytree(os.path.join(files_dir, 'böok$ collection'), '%s' % output_dir.join('böok$ collection'))
+
+    shutil.copytree('%s' % files_dir,
+                    '%s' % output_dir.join('böok$ collection'))
 
     with pytest.raises(SystemExit) as exc_info:
         copy_tree(input_dir='%s' % input_dir.join('böok$ collection'),
@@ -98,13 +114,17 @@ def test_copy_tree_overwrite_false(tmpdir):
 
 
 def test_copy_tree_overwrite_true(tmpdir):
-    input_dir = tmpdir.join('input')
+    input_dir = tmpdir.join('0')
     os.makedirs('%s' % input_dir)
-    copytree(os.path.join(files_dir, 'böok$ collection'), '%s' % input_dir.join('böok$ collection'))
 
-    output_dir = tmpdir.join('target')
+    shutil.copytree('%s' % files_dir,
+                    '%s' % input_dir.join('böok$ collection'))
+
+    output_dir = tmpdir.join('1')
     os.makedirs('%s' % output_dir)
-    copytree(os.path.join(files_dir, 'böok$ collection'), '%s' % output_dir.join('böok$ collection'))
+
+    shutil.copytree('%s' % files_dir,
+                    '%s' % output_dir.join('böok$ collection'))
 
     copy_tree(input_dir='%s' % input_dir.join('böok$ collection'),
               output_dir='%s' % output_dir.join('böok$ collection'),
@@ -115,6 +135,5 @@ def test_copy_tree_overwrite_true(tmpdir):
 
 
 def test_get_file_paths_from_directory():
-    return_value = get_file_paths_from_directory(dir_path=files_dir)
-    assert (os.path.join(files_dir, 'my_textfile.txt') in return_value) is True
-    assert (os.path.join(files_dir, 'some_subdir', 'IMG_1248.JPG') in return_value) is True
+    return_value = get_file_paths_from_directory(dir_path='%s' % files_dir)
+    assert ('%s' % files_dir.join('IMG_1248.JPG') in return_value) is True
--- Tasks/DropPy.Common/test_task_tools.py
+++ Tasks/DropPy.Common/test_task_tools.py
@@ -3,24 +3,25 @@
 
 from __future__ import unicode_literals
 import os
-from shutil import copyfile, copytree
+import py
+import shutil
 from task_tools import get_original_paths, pass_input_to_output
 
-files_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
+files_dir = py.path.local(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
 
 
 def test_get_original_paths(tmpdir):
     source_dir = tmpdir.join('-1')
     os.makedirs('%s' % source_dir)
 
-    copyfile(os.path.join(files_dir, 'this_side_up.png'), '%s' % source_dir.join('this_side_up.png'))
-    copytree(os.path.join(files_dir, 'äudio collection'), '%s' % source_dir.join('äudio collection'))
+    shutil.copyfile('%s' % files_dir.join('this_side_up.png'),
+                    '%s' % source_dir.join('this_side_up.png'))
 
     input_dir = tmpdir.join('0', 'files')
     os.makedirs('%s' % input_dir)
 
-    os.symlink('%s' % source_dir.join('this_side_up.png'), '%s' % input_dir.join('this_side_up_0.png'))
-    os.symlink('%s' % source_dir.join('äudio collection'), '%s' % input_dir.join('äudio collection 0'))
+    os.symlink('%s' % source_dir.join('this_side_up.png'),
+               '%s' % input_dir.join('this_side_up_0.png'))
 
     output_dir = tmpdir.join('1')
     os.makedirs('%s' % output_dir)
@@ -28,13 +29,23 @@ def test_get_original_paths(tmpdir):
     return_value = get_original_paths(input_or_output_dir='%s' % output_dir)
 
     assert ('%s' % source_dir.join('this_side_up.png') in return_value) is True
-    assert ('%s' % source_dir.join('äudio collection') in return_value) is True
 
 
 def test_pass_input_to_output(tmpdir):
-    pass_input_to_output(input_dir=[os.path.join(files_dir, 'some_subdir', 'IMG_1248.JPG'),
-                                    os.path.join(files_dir, 'äudio collection')],
-                         output_dir='%s' % tmpdir)
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
 
-    assert tmpdir.join('IMG_1248.JPG').check() is True
-    assert tmpdir.join('äudio collection').check() is True
+    shutil.copyfile('%s' % files_dir.join('pg5903.epub'),
+                    '%s' % input_dir.join('pg5903.epub'))
+
+    input_sub_dir = input_dir.join('foo')
+    os.makedirs('%s' % input_sub_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    pass_input_to_output(input_dir='%s' % input_dir,
+                         output_dir='%s' % output_dir)
+
+    assert output_dir.join('pg5903.epub').check() is True
+    assert output_dir.join('foo').check() is True
