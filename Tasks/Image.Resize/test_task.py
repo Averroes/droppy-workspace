0cd5f0ba864aa71d5c5f2b88288271af786aa6c5
Günther Eberl <guenther@eberl.se>
2017-12-25 19:08:57 +0100

    Fixed tests for Task Image.Resize
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/Image.Resize/test_task.py
+++ Tasks/Image.Resize/test_task.py
@@ -7,29 +7,44 @@ try:
     import Image
 except ImportError:
     from PIL import Image
+import py
 import pytest
+import shutil
 import task
 
-files_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
+files_dir = py.path.local(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
 
 
-def test_init(tmpdir):
-    t = task.Task(input_dir=[],
-                  output_dir='%s' % tmpdir)
+def test_input_empty(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir)
 
     assert isinstance(t, object)
 
 
-def test_passing_files(tmpdir):
-    input_paths = [os.path.join(files_dir, 'some_subdir', 'IMG_1248.JPG')]
+def test_input_file(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
 
-    t = task.Task(input_dir=input_paths,
-                  output_dir='%s' % tmpdir,
+    shutil.copyfile('%s' % files_dir.join('IMG_1248.JPG'),
+                    '%s' % input_dir.join('IMG_1248.JPG'))
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir,
                   width=900,
                   height=200,
                   filter='nearest')
 
-    resized_image = Image.open('%s' % tmpdir.join('IMG_1248.JPG'))
+    resized_image = Image.open('%s' % output_dir.join('IMG_1248.JPG'))
     width, height = resized_image.size
 
     assert width == 900
@@ -37,11 +52,15 @@ def test_passing_files(tmpdir):
 
 
 def test_invalid_filter_arg(tmpdir):
-    input_paths = [os.path.join(files_dir, 'some_subdir', 'IMG_1248.JPG')]
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
 
     with pytest.raises(SystemExit) as exc_info:
-        t = task.Task(input_dir=input_paths,
-                      output_dir='%s' % tmpdir,
+        t = task.Task(input_dir='%s' % input_dir,
+                      output_dir='%s' % output_dir,
                       width=900,
                       height=200,
                       filter='this filter does not exist')
