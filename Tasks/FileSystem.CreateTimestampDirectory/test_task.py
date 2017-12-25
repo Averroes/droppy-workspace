41571db4befffb376e44c0928939be837889c65d
Günther Eberl <guenther@eberl.se>
2017-12-25 17:26:50 +0100

    Fixed tests for Task FileSystem.CreateTimestampDirectory
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/FileSystem.CreateTimestampDirectory/test_task.py
+++ Tasks/FileSystem.CreateTimestampDirectory/test_task.py
@@ -3,40 +3,60 @@
 
 from __future__ import unicode_literals
 import os
+import py
 import pytest
+import shutil
 import task
 import datetime
 
-files_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
+files_dir = py.path.local(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
 
 
-def test_init(tmpdir):
-    t = task.Task(input_dir=[],
-                  output_dir='%s' % tmpdir)
+def test_input_empty(tmpdir):
+    input_dir = tmpdir.join('0', 'files')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir)
 
     assert isinstance(t, object)
 
 
-def test_passing_files(tmpdir):
-    input_paths = [os.path.join(files_dir, 'böok$ collection', 'pg5903.epub'),
-                   os.path.join(files_dir, 'böok$ collection', 'vom gut und böse.epub')]
+def test_input_file(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    shutil.copyfile('%s' % files_dir.join('pg5903.epub'),
+                    '%s' % input_dir.join('pg5903.epub'))
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
     name_pattern = '%Y-%m'
 
-    t = task.Task(input_dir=input_paths,
-                  output_dir='%s' % tmpdir,
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir,
                   name_pattern=name_pattern)
 
     d = datetime.datetime.now()
     year_month = d.strftime(name_pattern)
 
-    assert tmpdir.join(year_month, 'pg5903.epub').check() is True
-    assert tmpdir.join(year_month, 'vom gut und böse.epub').check() is True
+    assert output_dir.join(year_month, 'pg5903.epub').check() is True
 
 
 def test_len_zero_name_pattern_arg(tmpdir):
+    input_dir = tmpdir.join('0', 'files')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
     with pytest.raises(SystemExit) as exc_info:
-        t = task.Task(input_dir=[],
-                      output_dir='%s' % tmpdir,
+        t = task.Task(input_dir='%s' % input_dir,
+                      output_dir='%s' % output_dir,
                       name_pattern='')
 
     assert exc_info.type == SystemExit
