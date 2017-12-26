8f0db8f7520f477c0c2e5554a47752dd58b7dedc
Günther Eberl <guenther@eberl.se>
2017-12-26 10:48:03 +0100

    Fixed tests for Task Web.ImgurUpload
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/Web.ImgurUpload/test_task.py
+++ Tasks/Web.ImgurUpload/test_task.py
@@ -2,21 +2,34 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+import os
 import pytest
 import task
 
 
-def test_init(tmpdir):
-    t = task.Task(input_dir=[],
-                  output_dir='%s' % tmpdir,
+def test_input_empty(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir,
                   client_id='some invalid client id')
 
     assert isinstance(t, object)
 
 
 def test_missing_client_id_arg(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
     with pytest.raises(SystemExit) as exc_info:
-        t = task.Task(input_dir=[],
-                      output_dir='%s' % tmpdir)
+        t = task.Task(input_dir='%s' % input_dir,
+                      output_dir='%s' % output_dir)
 
     assert exc_info.type == SystemExit
