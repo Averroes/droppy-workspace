c190eaea938d13e2f963550e6d1df79781aa2b8f
Günther Eberl <guenther@eberl.se>
2017-12-26 10:14:07 +0100

    Fixed tests for Task Markdown.AddToc
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/Markdown.AddToc/test_task.py
+++ Tasks/Markdown.AddToc/test_task.py
@@ -3,26 +3,41 @@
 
 from __future__ import unicode_literals
 import codecs
+import py
 import os
+import shutil
 import task
 
-files_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
+files_dir = py.path.local(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
 
 
-def test_init(tmpdir):
-    t = task.Task(input_dir=[],
-                  output_dir='%s' % tmpdir)
+def test_input_empty(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir)
 
     assert isinstance(t, object)
 
 
-def test_passing_files(tmpdir):
-    input_paths = [os.path.join(files_dir, 'some_subdir', 'sämple.md')]
+def test_input_file(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
 
-    t = task.Task(input_dir=input_paths,
-                  output_dir='%s' % tmpdir)
+    shutil.copyfile('%s' % files_dir.join('sämple.md'),
+                    '%s' % input_dir.join('sämple.md'))
 
-    output_file = tmpdir.join('sämple.md')
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir)
+
+    output_file = output_dir.join('sämple.md')
     lines = codecs.open('%s' % output_file, encoding='utf-8', mode='r').readlines()
 
     assert lines[0] == '# Table of Contents\n'
