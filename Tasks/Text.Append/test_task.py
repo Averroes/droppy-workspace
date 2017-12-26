309d379d0adea9a5e5177155fe23cd73b6ecac48
Günther Eberl <guenther@eberl.se>
2017-12-26 10:23:56 +0100

    Fixed tests for Task Text.Append
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/Text.Append/test_task.py
+++ Tasks/Text.Append/test_task.py
@@ -3,38 +3,60 @@
 
 from __future__ import unicode_literals
 import codecs
+import py
 import pytest
 import os
+import shutil
 import task
 
-files_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
+files_dir = py.path.local(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
 
 
-def test_init(tmpdir):
-    t = task.Task(input_dir=[],
-                  output_dir='%s' % tmpdir,
+def test_input_empty(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir,
                   target_file='%s' % tmpdir.join('output.txt'))
 
     assert isinstance(t, object)
 
 
 def test_unfilled_target_file_arg(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
     with pytest.raises(SystemExit) as exc_info:
-        t = task.Task(input_dir=[],
-                      output_dir='%s' % tmpdir)
+        t = task.Task(input_dir='%s' % input_dir,
+                      output_dir='%s' % output_dir)
 
     assert exc_info.type == SystemExit
 
 
 def test_passing_files(tmpdir):
-    input_paths = [os.path.join(files_dir, 'my_textfile.txt'),
-                   os.path.join(files_dir, 'täxt datei.txt')]
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
 
-    t = task.Task(input_dir=input_paths,
+    file_one = input_dir.join('foo.txt')
+    file_one.write('baz\n')
+    file_two = input_dir.join('bar.txt')
+    file_two.write('bar\n')
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
                   output_dir='%s' % tmpdir,
                   target_file='%s' % tmpdir.join('output.txt'))
 
     assert tmpdir.join('output.txt').check() is True
 
     output_file_lines = codecs.open('%s' % tmpdir.join('output.txt'), encoding='utf-8', mode='r').readlines()
-    assert len(output_file_lines) == 5
+    assert len(output_file_lines) == 4
