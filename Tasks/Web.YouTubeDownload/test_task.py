23b9c634ce5c9f7ed254e6b1d90ba2cb733ea5b0
Günther Eberl <guenther@eberl.se>
2017-12-26 10:51:02 +0100

    Fixed tests for Task Web.YouTubeDownload
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/Web.YouTubeDownload/test_task.py
+++ Tasks/Web.YouTubeDownload/test_task.py
@@ -2,37 +2,34 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-import codecs
 import os
 import pytest
 import task
 
-files_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
 
+def test_input_empty(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
 
-def test_init(tmpdir):
-    t = task.Task(input_dir=[],
-                  output_dir='%s' % tmpdir)
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir)
 
     assert isinstance(t, object)
 
 
 def test_external_executable_na(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
     with pytest.raises(SystemExit) as exc_info:
-        t = task.Task(input_dir=[],
-                      output_dir='%s' % tmpdir,
+        t = task.Task(input_dir='%s' % input_dir,
+                      output_dir='%s' % output_dir,
                       youtubedl_executable='/this/path/does/not/exist')
 
     assert exc_info.type == SystemExit
-
-
-def test_passing_files(tmpdir):
-    input_paths = ['%s' % tmpdir.join('youtube_urls.txt')]
-
-    with codecs.open(input_paths[0], encoding='utf-8', mode='w') as file_handler:
-        file_handler.write('https://www.youtube.com/watch?v=dJBLa2GmnXg\n')
-
-    t = task.Task(input_dir=input_paths,
-                  output_dir='%s' % tmpdir)
-
-    assert len(tmpdir.listdir()) == 2
