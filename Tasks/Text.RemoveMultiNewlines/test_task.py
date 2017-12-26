857f38ef167bec7af3d280c30669d40735e56ffd
Günther Eberl <guenther@eberl.se>
2017-12-26 10:41:39 +0100

    Fixed tests for Task Text.RemoveMultiNewlines
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- Tasks/Text.RemoveMultiNewlines/test_task.py
+++ Tasks/Text.RemoveMultiNewlines/test_task.py
@@ -3,22 +3,44 @@
 
 from __future__ import unicode_literals
 import codecs
+import py
 import os
 import task
 
-files_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
+files_dir = py.path.local(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'Test', 'files'))
 
 
-def test_init(tmpdir):
-    t = task.Task(input_dir=[],
-                  output_dir='%s' % tmpdir)
+def test_input_empty(tmpdir):
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir)
 
     assert isinstance(t, object)
 
 
 def test_passing_files(tmpdir):
-    t = task.Task(input_dir=[os.path.join(files_dir, 'my_textfile.txt')],
-                  output_dir='%s' % tmpdir)
+    input_dir = tmpdir.join('0')
+    os.makedirs('%s' % input_dir)
 
-    output_file_lines = codecs.open('%s' % tmpdir.join('my_textfile.txt'), encoding='utf-8', mode='r').readlines()
-    assert len(output_file_lines) == 2
+    file_one = input_dir.join('foo.txt')
+    file_one.write('baz\n')
+
+    file_two = input_dir.join('bar.txt')
+    file_two.write('bar\n\n\n\n\nfoo')
+
+    output_dir = tmpdir.join('1')
+    os.makedirs('%s' % output_dir)
+
+    t = task.Task(input_dir='%s' % input_dir,
+                  output_dir='%s' % output_dir)
+
+    output_file_one_lines = codecs.open('%s' % output_dir.join('foo.txt'), encoding='utf-8', mode='r').readlines()
+    assert len(output_file_one_lines) == 1
+
+    output_file_two_lines = codecs.open('%s' % output_dir.join('bar.txt'), encoding='utf-8', mode='r').readlines()
+    assert len(output_file_two_lines) == 3
